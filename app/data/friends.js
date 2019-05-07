8143fda3b9bcfa11f1a91d82ad86d4602084781f
Jon <jon.ledbetter@gmail.com>
2019-05-07 09:39:50 -0400

    still need to add images / clear inputs

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,13 +14,32 @@ var friends = [
                 4, 4, 2, 4, 3, 3, 5, 2, 5, 3
             ]
     }, {
-
-    }, {
         "name": "Oscar",
         "photo": "..data/img/oscar.jpeg",
         "scores":
             [
                 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+            ]
+    }, {
+        "name": "Newt",
+        "photo": "..data/img/newt.jpeg",
+        "scores":
+            [
+                1, 2, 3, 4, 5, 4, 3, 2, 5, 1
+            ]
+    }, {
+        "name": "Phil",
+        "photo": "..data/img/phil.jpeg",
+        "scores":
+            [
+                5, 4, 3, 2, 1, 2, 3, 4, 5, 1
+            ]
+    }, {
+        "name": "Ted",
+        "photo": "..data/img/ted.jpeg",
+        "scores":
+            [
+                5, 5, 5, 5, 5, 5, 5, 5, 5, 5
             ]
     }
 ]
